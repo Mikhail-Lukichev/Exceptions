5f8a97c3bda9efae417ebfbafc0c5a1880372147
Mikhail Lukichev <dextrumm@gmail.com>
2023-08-22 21:49:30 -0600

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
+import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void checkData(String login, String password, String confirmPassword) {
+        // check login
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9_]");
+        Matcher matcher = pattern.matcher(login);
+        boolean matchFound = matcher.find();
+        if (login.length() > 20 || matchFound) {
+            throw new WrongLoginException();
+        }
+
+        // check password
+        pattern = Pattern.compile("[^a-zA-Z0-9_]");
+        matcher = pattern.matcher(password);
+        matchFound = matcher.find();
+        if (password.length() > 20 || matchFound || !password.equals(confirmPassword)) {
+            throw new WrongPasswordException();
+        }
+
+        System.out.println("Login: " + login + "; password: " + password);
+    }
+
+    public static void main(String[] args) {
+        String login = "L0g1n_";
+        String password = "pass_w0rD";
+        String confirmPassword = "pass_w0rD";
+        try {
+            checkData(login, password, confirmPassword);
+        } catch (WrongLoginException e) {
+            System.out.println("Incorrect login");
+        } catch (WrongPasswordException e) {
+            System.out.println("Incorrect password");
+        } finally {
+            System.out.println("Data is checked");
+        }
     }
 }
